696db9c2db0fe77bea447432d12076200fcc9c9e
TylerD07 <tyler.daughtry@aol.com>
2018-10-04 18:36:54 -0400

    fineshed everything up to the update function, need to fix if else statement

--- EmployeeApp/MyWork/app.js
+++ EmployeeApp/MyWork/app.js
@@ -21,22 +21,35 @@ const add = function () {
 
 };
 
-// const verify = function () {
-//     $("#verifyButton").on("click", function () {
-//         let employeeName = $("#nameInput2").value;
-//         console.log("employeeName");
-//     })
-// };
+const verify = function () {
+    $("#verifyButton").on("click", function () {
+        $("#visiblePage").empty();
+        let employeeName = document.getElementById("nameInput2").value;
+        for (let i = 0; i < employeeList.length; i++) {
+            if(employeeName == employeeList[i].name){
+                $("#visiblePage").append('<p class= "confirm"> Yes, this employee is in the system.</p>');
+            }
+            else{
+                $("#visiblePage").append("<p class ='invalid> Employee name is not valid.</p>");
+            } // fix else statement
+            
+        }
+    })
+};
 
+const update = function(){
+
+};
 
 //Executions
 
 $("#view").on("click", function () {
     $("#visiblePage").empty();
     $("#select").addClass("hideForm");
+    $(".updateInput").addClass("showTextbox");
     $("#inputBarID").addClass("inputBar");
 
-    // $(".inputBar").toggleClass( "inputBar", false );
+
 
     renderList();
 });
@@ -44,6 +57,7 @@ $("#view").on("click", function () {
 $("#add").on("click", function () {
     $("#visiblePage").empty();
     $("#select").addClass("hideForm");
+    $(".updateInput").addClass("showTextbox");
     $(".inputBar").toggleClass("inputBar", false);
 })
 
@@ -52,9 +66,21 @@ add();
 $("#verify").on("click", function () {
     $("#visiblePage").empty();
     $("#inputBarID").addClass("inputBar");
+    $(".updateInput").addClass("showTextbox");
     $("#select").removeClass("hideForm");
 
 })
 
 
-// verify();
+verify();
+
+$("#update").on("click", function() {
+    $("#visiblePage").empty();
+    $("#select").addClass("hideForm");
+    $("#inputBarID").addClass("inputBar");
+    $(".updateInput").removeClass("showTextbox");
+
+});
+
+
+    
